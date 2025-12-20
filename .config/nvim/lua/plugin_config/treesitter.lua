8e97ff01b9da0752d4f84e3282a0cbddf45292e1
arjanmc <arjanchakravarthy22@gmail.com>
2025-12-19 20:49:43 -0500

    nit

--- .config/nvim/lua/plugin_config/treesitter.lua
+++ .config/nvim/lua/plugin_config/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
   ensure_installed = {"c", "lua", "rust", "ruby", "vim", "python", "bash"},
 
   autotag = { enable = true },
